a288e77878dd4f99b54cd66a7546bc08c4dcc07e
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-04-10 10:53:08 +0200

    Trying to provoke conflick

--- JohanneMicroMath/JohanneMicroMath.cs
+++ JohanneMicroMath/JohanneMicroMath.cs
@@ -18,5 +18,18 @@ namespace JohanneMicroMath
         {
             return t1 + t2;
         }
+
+        /// <summary>
+        /// Add three numbers
+        /// </summary>
+        /// <param name="t1">Fist number</param>
+        /// <param name="t2">Second number</param>
+        /// <param name="t3">Third number</param>
+        /// <returns></returns>
+        public int AddThreeNumbers(int t1, int t2, int t3)
+        {
+            return t1 + t2+t3; //Add
+        }
+
     }
 }
