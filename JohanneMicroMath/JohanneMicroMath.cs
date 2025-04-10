90e38ef6715644103d9dc28e09cc1b51efc1d2f0
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-04-10 10:41:10 +0200

    Add description for add

--- JohanneMicroMath/JohanneMicroMath.cs
+++ JohanneMicroMath/JohanneMicroMath.cs
@@ -8,6 +8,12 @@ namespace JohanneMicroMath
 {
     public class JohanneMicroMath
     {
+        /// <summary>
+        /// Add two numbers
+        /// </summary>
+        /// <param name="t1">First number</param>
+        /// <param name="t2">Second number</param>
+        /// <returns>return the result as an int</returns>
         public int Add(int t1, int t2) 
         {
             return t1 + t2;
